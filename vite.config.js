c235a0b60adf66b7d1ae702d98d35b21f8bcceaa
olamilekan003-star <owolabiolamilekan071@gmail.com>
2025-09-15 12:36:38 +0100

    chore: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/com-lekdas/',   // 👈 this is required for GitHub Pages
 })
